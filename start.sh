d8601f81881a79cbed4de818f522f7119bdcd0e2
Mido-Egypt <omarnageh319@gmail.com>
2026-02-01 21:59:22 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-SID=test96775
+# توليد SID عشوائي (8 حروف + أرقام)
+SID=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 8)
+
+# الرابط الأساسي
 BASE_URL=https://scope-batch-continuous-wishes.trycloudflare.com
 
 echo "=========================="
+echo "SID المستخدم : $SID"
 echo "رابط المراقب : $BASE_URL/owner/$SID"
 echo "رابط البث    : $BASE_URL/stream/$SID"
 echo "=========================="
